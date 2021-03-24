747927a7c0996fbc5c7e433fc76c59535990a0ef
Kimberly Lin <kimberlylin2014@gmail.com>
2021-03-24 10:56:03 -0700

    start on docker setup and implement basic k6 testing

--- model/product.js
+++ model/product.js
@@ -97,7 +97,7 @@ const getStylesByProductId = (productId, callback) => {
 
   // Option 3
   const styleQuery = `WITH styles AS (
-    SELECT * FROM styles WHERE product_id = ${productId}
+    SELECT * FROM styles WHERE product_id = 14032
   ), photo AS (
     SELECT
       GROUP_CONCAT(photos.url) AS url,
