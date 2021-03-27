f127fff1865ef7acab85b9d683dee1f2977b8b26
Kimberly Lin <kimberlylin2014@gmail.com>
2021-03-27 13:12:09 -0700

    updated ip address

--- devTest/basic-test.js
+++ devTest/basic-test.js
@@ -2,12 +2,42 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 10,
-  duration: '30s'
+  vus: 500,
+  duration: '5s'
 }
 
+// export default function () {
+//   let res = http.get('http://localhost:3000/products/1400032/styles');
+//   console.log(`RESPONSE TIME: ${res.timings.duration} ms`)
+//   sleep(1);
+// }
+
+// export let options = {
+//   stages: [
+//     { duration: '5s', target: 100 }, // below normal load
+//     { duration: '5s', target: 200 },
+//     { duration: '2s', target: 300 }, // normal load
+//     { duration: '5s', target: 500 },
+//     { duration: '2s', target: 1000 }, // around the breaking point
+//     { duration: '5s', target: 2000 },
+//     { duration: '2s', target: 3000 }, // beyond the breaking point
+//     { duration: '5s', target: 4000 },
+//     { duration: '5s', target: 900 },
+//     { duration: '5s', target: 300 },
+//     { duration: '10s', target: 0 }, // scale down. Recovery stage.
+//   ],
+// };
+
+
+
 export default function () {
-  let res = http.get('http://localhost:3000/products/14032/styles');
-  console.log(`RESPONSE TIME: ${res.timings.duration} ms`)
-  sleep(1);
+  try {
+    let res = http.get('http://localhost:3000/products/9090/related');
+    console.log(`RESPONSE TIME: ${res.timings.duration} ms`)
+    sleep(1);
+  } catch (error) {
+    newrelic.noticeError(error);
+  }
+
 }
+
