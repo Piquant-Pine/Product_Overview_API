09acbdd678ce4e869373b03183076598368f9bfb
Kimberly Lin <kimberlylin2014@gmail.com>
2021-06-13 17:46:15 -0700

    update comment to clarify

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-// require('newrelic');
 const express = require('express');
 const connection = require('./mysql/connection.js')
 const app = express();
@@ -9,22 +8,17 @@ const router = require('./router/router.js');
 const PORT = 3000;
 const path = require('path');
 
-// serving loader.io static file
+// serving loader.io for stress testing
 app.use(express.static('public'));
 
-// require('./csv10/styles.etl.js');
-// Seeding Data (uncomment to run)
+// UNCOMMENT TO SEED DATA FROM CSV FILES
 // require('./csv/styles.etl.js')
-// const product = require('./csv/product.etl.js');
-// const feature = require('./csv/features.etl.js');
-// const styles = require('./csv/styles.etl.js');
-// const skus = require('./csv/skus.etl.js');
+// require('./csv/product.etl.js');
+// require('./csv/features.etl.js');
+// require('./csv/styles.etl.js');
+// require('./csv/skus.etl.js');
 
 app.use('/products', router)
-
-app.get('/test', (req, res) => {
-  res.send('testing potato')
-})
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
