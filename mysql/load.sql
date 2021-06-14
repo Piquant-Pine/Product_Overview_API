759a2db3b74801a9987146e7aaa56825afc0db17
Kimberly Lin <kimberlylin2014@gmail.com>
2021-06-13 17:49:42 -0700

    remove duplicate comments

--- mysql/load.sql
+++ mysql/load.sql
@@ -1,35 +1,34 @@
--- USE sdc;
+-- TESTING FILES
 
--- Finalized
+USE sdc;
+
 LOAD DATA local INFILE './csv10/product.test.csv'
 INTO TABLE Products
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
--- Finalized
--- SET foreign_key_checks = 0;
--- LOAD DATA local INFILE './csvClean/features.csv'
--- INTO TABLE Features
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n';
+SET foreign_key_checks = 0;
+LOAD DATA local INFILE './csvClean/features.csv'
+INTO TABLE Features
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
 
--- IN PROGRESS
--- SET foreign_key_checks = 0;
--- LOAD DATA local INFILE './csv10/styles.test.csv'
--- INTO TABLE Styles
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+SET foreign_key_checks = 0;
+LOAD DATA local INFILE './csv10/styles.test.csv'
+INTO TABLE Styles
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
--- LOAD DATA local INFILE './csv10/skus.test.csv'
--- INTO TABLE SKUs
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
+LOAD DATA local INFILE './csv10/skus.test.csv'
+INTO TABLE SKUs
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
 
 LOAD DATA local INFILE './csv10/photos.test.csv'
 INTO TABLE Photos
@@ -44,48 +43,3 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
-
--- USE sdc;
-
--- LOAD DATA local INFILE '../csv10/product.test.csv'
--- INTO TABLE Products
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
-
--- LOAD DATA local INFILE '../csv10/features.test.csv'
--- INTO TABLE Features
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA local INFILE '../csv10/styles.test.csv'
--- INTO TABLE Styles
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA local INFILE '../csv10/skus.test.csv'
--- INTO TABLE SKUs
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA local INFILE '../csv10/photos.test.csv'
--- INTO TABLE Photos
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA local INFILE '../csv10/related.test.csv'
--- INTO TABLE Related_Product
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
